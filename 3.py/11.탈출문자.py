b78b1002d408e1c8432a9871e528410b02d21485
yubi5co <80345968+yubi5co@users.noreply.github.com>
2021-03-10 01:50:17 +0900

    Delete 3.py directory

--- 3.py/11.탈출문자.py
+++
@@ -1,7 +0,0 @@
-#탈출문자
-print('백문이 불여일견\n백견이 불여일타') # \n :줄바꿈
-print('저는 \"나도코딩\" 입니다.')       # \" :"출력가능
-print('C:\\Users\\sksks\\Desktop\\pythonworkspace>') # \\두개의 역슬러시: 문장내에서 \하나의 역슬러시로 출력
-print('red apple\rpine') #\r :커서를 맨 앞으로 이동
-print('redd\bapple') #백스페이스(한 글자(d) 삭제)
-print('red\tapple')  #\t :탭역할
